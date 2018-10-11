b10411f51d09d50e8f7609cc6394ee06caadeefa
emedinaserra1 <44063077+emedinaserra1@users.noreply.github.com>
2018-10-11 11:08:58 -0500

    Update google-sheets-html.js

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -16,7 +16,7 @@ var visualization;
 
 function drawVisualization() {
     var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1JJUBz6oa5ORsnqZvoRig1Gh6pCslBc97XQADlPPtjxk&output=html&usp=sharing');
-    query.setQuery('SELECT A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U label A "Duration", B "Song", C "Requested By", D "URL"');
+    query.setQuery('SELECT A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U label A "StartDate", B "dbegin", C "Campus", D "Room", E "Title", F "CRN", G "Term", H "Dept", I "Section", J "Hours", K "Fee", L "EndDate", M "#ses", N "#Days", O "StartTime", P "EndTime", Q "Instructor", R "Resp" , S "Enrl*20181010", T"Real-time", U"Bookstore" ');
     query.send(handleQueryResponse);
 }
 
